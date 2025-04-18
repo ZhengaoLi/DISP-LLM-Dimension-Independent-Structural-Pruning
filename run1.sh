7365111676255d2cd0f4c34f598767edae102cf0
gaosh <gao.shanqian@gmail.com>
2025-04-17 20:37:17 -0400

    updates training lr

--- run1.sh
+++ run1.sh
@@ -7,7 +7,7 @@ torchrun --nproc_per_node=2 train_hypernetwork.py \
     --batch_size 1 \
     --total_n_step 10000 \
     --hn_lr 1e-3 \
-    --min_hn_lr 1e-4 \
+    --min_hn_lr 1e-3 \
     --use_sch False \
     --out_dir ./output \
     --exp_name "PruneLlama"
